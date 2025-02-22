9504b434524d6ba8ac13161a137d1c5eb7a88cc2
Chris Carter <108958713+chrisjamiecarter@users.noreply.github.com>
2025-02-22 19:23:18 +0000

    Added section 3 project 3 signup form

---
+++ section-03/03-signup-form/src/App.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+function App() {
+  function handleSubmit(event: React.FormEvent) {
+    event.preventDefault();
+    const formElement = event.currentTarget as HTMLFormElement;
+    const formData = new FormData(formElement);
+    const email = formData.get("email");
+    console.log("email", email);
+    formElement.reset();
+  }
+
+  return (
+    <section>
+      <h1>Signup form</h1>
+      <form onSubmit={handleSubmit} method="post">
+        <label htmlFor="email">Email:</label>
+        <input
+          id="email"
+          type="email"
+          name="email"
+          placeholder="example@email.com"
+        />
+        <br />
+
+        <label htmlFor="password">Password:</label>
+        <input id="password" type="password" name="password" />
+        <br />
+
+        <button type="submit">Signup</button>
+      </form>
+    </section>
+  );
+}
+
+export default App;
