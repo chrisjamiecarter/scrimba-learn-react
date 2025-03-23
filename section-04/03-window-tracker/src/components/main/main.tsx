7f827aac72d329da78516c8d694f6b68f6b8e520
Chris Carter <108958713+chrisjamiecarter@users.noreply.github.com>
2025-03-23 14:43:38 +0000

    Implemented toggle button

--- section-04/03-window-tracker/src/components/main/main.tsx
+++ section-04/03-window-tracker/src/components/main/main.tsx
@@ -1,10 +1,17 @@
+import { useState } from "react";
 import WindowTracker from "../window-tracker/window-tracker";
 
 const Main = () => {
+  const [show, setShow] = useState<boolean>(false);
+
+  const toggleShow = () => {
+    setShow((prev) => !prev);
+  };
+
   return (
     <main className="container">
-      <button>Toggle WindowTracker</button>
-      <WindowTracker />
+      <button onClick={toggleShow}>Toggle WindowTracker</button>
+      {show ? <WindowTracker /> : null}
     </main>
   );
 };
