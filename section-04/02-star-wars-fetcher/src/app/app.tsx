91fdb2071cb8dc119f212315dffe9d09ec2d24d9
Chris Carter <108958713+chrisjamiecarter@users.noreply.github.com>
2025-03-23 13:50:19 +0000

    Corrected useEffect usage

--- section-04/02-star-wars-fetcher/src/app/app.tsx
+++ section-04/02-star-wars-fetcher/src/app/app.tsx
@@ -1,14 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const App = () => {
-  const [starWarsData, setStarWarsData] = useState(null);
+  const [count, setCount] = useState<number>(0);
+  const [starWarsData, setStarWarsData] = useState({});
 
-  // fetch("https://swapi.dev/api/people/1")
-  //   .then((result) => result.json())
-  //   .then((data) => setStarWarsData(data));
+  console.log("Rendered!");
+
+  useEffect(() => {
+    console.log("Effect ran!");
+    fetch("https://swapi.dev/api/people/1")
+      .then((result) => result.json())
+      .then((data) => setStarWarsData(data));
+  }, []);
 
   return (
     <div>
+      <h2>The count is {count}</h2>
+      <button onClick={() => setCount((prev) => prev + 1)}>Add</button>
       <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
     </div>
   );
