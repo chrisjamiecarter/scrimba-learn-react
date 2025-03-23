73cbd8110092ac7dee5c9fc685ed059efdde20ab
Chris Carter <108958713+chrisjamiecarter@users.noreply.github.com>
2025-03-23 15:01:47 +0000

    Added event listener and cleanup

--- section-04/03-window-tracker/src/components/window-tracker/window-tracker.tsx
+++ section-04/03-window-tracker/src/components/window-tracker/window-tracker.tsx
@@ -1,5 +1,21 @@
+import { useEffect, useState } from "react";
+
 const WindowTracker = () => {
-  return <h1>Window width: {window.innerWidth}</h1>;
+  const [windowWidth, setWindowWidth] = useState<number>(window.innerWidth);
+
+  useEffect(() => {
+    function watchWindowWidth() {
+      console.log("Resized");
+      setWindowWidth(window.innerWidth);
+    }
+    window.addEventListener("resize", watchWindowWidth);
+    return function () {
+      console.log("Cleaning up");
+      window.removeEventListener("resize", watchWindowWidth);
+    };
+  }, []);
+
+  return <h1>Window width: {windowWidth}</h1>;
 };
 
 export default WindowTracker;
